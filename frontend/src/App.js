9ecb7030d36299b4182d299b8a965d654f455353
Nitin Thwass <thwassnitin619@gmail.com>
2022-05-21 22:15:07 +0530

    Testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <h1>Welcome to Testing</h1>
         </Container>
       </main>
 
